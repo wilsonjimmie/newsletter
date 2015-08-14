d1922103cd15b97795baaec653e94b1959bb744f
Jimmie Johansson <jimmie@wilsoncreative.se>
2015-08-14 14:03:34 +0200

    Update request types in routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,27 +2,19 @@
 
 Route::group(['prefix' => 'newsletter'], function() {
     Route::get('/', 'NewsletterController@index');
-    //Route::post('/add/{newsletter}', 'NewsletterController@store');
-    //Route::put('/update/{newsletter}', 'NewsletterController@update');
     Route::get('/find/{id}', 'NewsletterController@show');
-    Route::get('/delete/{id}', 'NewsletterController@destroy');
+    Route::delete('/delete/{id}', 'NewsletterController@destroy');
 });
 
 Route::group(['prefix' => 'list'], function() {
     Route::get('/', 'MailinglistController@index');
-    //Route::post('/add/{newsletter}', 'MailinglistController@store');
-    //Route::put('/update/{newsletter}', 'MailinglistController@update');
     Route::get('/find/{id}', 'MailinglistController@show');
-    Route::get('/delete/{id}', 'MailinglistController@destroy');
+    Route::delete('/delete/{id}', 'MailinglistController@destroy');
 });
 
 Route::group(['prefix' => 'subscriber'], function() {
-    Route::get('/{list_id}', 'SubscriberController@index');
     Route::post('/add', 'SubscriberController@store');
-    //Route::put('/update', 'SubscriberController@update');
+    Route::get('/{list_id}', 'SubscriberController@index');
     Route::get('/{list_id}/find/{email}', 'SubscriberController@show');
-    Route::get('/{list_id}/delete/{email}', 'SubscriberController@destroy');
-
-    Route::get('/{list_id}/{email}/edit', 'SubscriberController@edit');
-    Route::get('/{list_id}/create', 'SubscriberController@create');
+    Route::delete('/{list_id}/delete/{email}', 'SubscriberController@destroy');
 });
